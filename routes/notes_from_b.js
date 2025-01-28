cfff011648a1bfc144ce693e966c5a6f722f2cf1
jojihonjo <honjo2227@yahoo.co.jp>
2025-01-28 22:00:51 +0900

    final update

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,24 +1,26 @@
 var express = require('express');
 var router = express.Router();
-const cors = require('cors');
+const cors = require('cors'); // corsミドルウェアを追加
 require('dotenv').config();
 
-
-//接続情報を設定
+// 接続情報を設定
 const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
+// corsミドルウェアを使用
+router.use(cors());
+
 router.get('/', async (req, res) => {
- // データベース、コレクションを指定
+// データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
 
-// idが１のドキュメントを取得
-const query = { id: 1};
-const note = await notes.findOne(query);
 
-res.json(note.title);
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
+
+res.json(note);
 })
 
 module.exports = router;
